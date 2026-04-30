9bf7306b2507eb873a02f964ef7b18f1ba3ce8e9
Olavo Neves <olavo9neves@gmail.com>
2026-04-30 11:50:57 -0300

    feature: Add initial API

---
+++ src/main/java/br/com/fiap/toytrack/ToytrackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.toytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToytrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/toytrack/ToytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.toytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
